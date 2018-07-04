e10de2d847434a5fe678501a49286e925e3107a3
Kanika sharma <sharmak821@gmail.com>
2018-07-04 13:49:42 +0530

    updated

--- app.js
+++ app.js
@@ -20,9 +20,9 @@ var   express       = require('express'),
 
 
 //===========================================================
-
+var url = process.env.DATABASEURL || "mongodb://localhost/JammuSite_fina"
  mongoose.connect(process.env.DATABASEURL); 
-// mongoose.connect("mongodb://Kanika:mummapapa21@ds231549.mlab.com:31549/cityoftemples"); 
+ 
     
   app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine","ejs");
